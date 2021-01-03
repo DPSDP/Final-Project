122e57333bde901d0f8173d8a70a10aec330394c
MounaBadouri <56407301+MounaBadouri@users.noreply.github.com>
2021-01-03 12:23:03 +0100

    MàJ de l'exo 3

---
+++ Exercice-3/Dices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Final_DSDP_EX3
+{
+    public class Dices
+    {
+        Dice Dice1 { get; set; }
+        Dice Dice2 { get; set; }
+        int res { get; set; }
+        public Dices()
+        {
+           this.res=this.RollingResult() ;
+        }
+        public  int RollingResult()
+        {
+            return Dice1.RollDice()+ Dice2.RollDice();
+
+        }
+    }
+}
