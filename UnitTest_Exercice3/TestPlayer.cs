4e76220694f4c63df16a8e49eb41e5785608a229
Nandy Bâ <nandyba@yahoo.fr>
2021-01-08 23:54:51 +0100

    Exercice 3 - PlayerConstructorWithAliasOnly unit test redaction

---
+++ UnitTest_Exercice3/TestPlayer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Projet_Final_DSDP_EX3;
+using System.Collections.Generic;
+
+namespace Projet_Final_DSDP_EX3
+{
+    [TestClass]
+    public class TestPlayer
+    {
+        [TestMethod]
+        public void TestPlayerConstructorWithAliasOnly()
+        {
+            Player p1 = new Player("Sarah");
+            Assert.AreEqual("Sarah", p1.alias);
+        }
+
+    }
+}
